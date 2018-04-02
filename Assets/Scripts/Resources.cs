13e7d66ee2856c80f084101cde27112be833a887
Joshua Rand <jrand35@aol.com>
2018-04-01 21:24:23 -0400

    Simple UI

---
+++ Assets/Scripts/Resources.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resources : MonoBehaviour {
+
+    public static Resources sharedInstance;
+    public int startingResourcePoints;
+    public int startingToolPoints;
+    private int resourcePoints;
+    private int toolPoints;
+
+    public int ResourcePoints
+    {
+        get
+        {
+            return resourcePoints;
+        }
+
+        set
+        {
+            resourcePoints = value;
+            UI.sharedInstance.UpdateResourcePointsText();
+        }
+    }
+    
+    public int ToolPoints
+    {
+        get
+        {
+            return toolPoints;
+        }
+
+        set
+        {
+            toolPoints = value;
+            UI.sharedInstance.UpdateToolPointsText();
+        }
+    }
+
+    void Awake()
+    {
+        resourcePoints = startingResourcePoints;
+        toolPoints = startingToolPoints;
+        if (sharedInstance == null)
+        {
+            sharedInstance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start () {
+
+    }
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour {
+
+    public static UI sharedInstance;
+    public Text resourcePointsText;
+    public Text toolPointsText;
+    
+	void Awake () {
+        if (sharedInstance == null)
+        {
+            sharedInstance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    void Start()
+    {
+        UpdateResourcePointsText();
+        UpdateToolPointsText();
+    }
+
+    public void UpdateResourcePointsText()
+    {
+        resourcePointsText.text = Resources.sharedInstance.ResourcePoints.ToString() + " Resource Points";
+    }
+
+    public void UpdateToolPointsText()
+    {
+        toolPointsText.text = Resources.sharedInstance.ToolPoints.ToString() + " Tool Points";
+    }
+
+    public void BuildTower()
+    {
+        if (Resources.sharedInstance.ResourcePoints >= 20)
+        {
+            Resources.sharedInstance.ResourcePoints -= 20;
+        }
+    }
+	
+	void Update () {
+		
+	}
+}
