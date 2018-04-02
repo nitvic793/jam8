ea37cedab355772223f4a31a7443e303d16be832
Nitish Victor <nithishvictor@gmail.com>
2018-04-02 02:06:05 -0400

    Unit and Enemy behaviors progress

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -10,32 +10,119 @@ enum EnemyStates
     DIE
 }
 
-public class EnemyBehavior : MonoBehaviour {
+public class EnemyBehavior : MonoBehaviour
+{
 
     NavMeshAgent navMesh;
+    Animator animator;
 
     EnemyStates CurrentState;
+    EnemyStates PreviousState;
 
-	void Start ()
+    bool isRunning = false;
+    bool isAttacking = false;
+    bool isIdle = true;
+    bool isDead = false;
+
+    float attackCycleTime = 0F;
+
+    public float Health = 100F;
+    public float attackDistance = 1F;
+
+    void Start()
     {
         navMesh = GetComponent<NavMeshAgent>();
-        CurrentState = EnemyStates.PURSUE;
-	}
-	
-	void Update ()
+        animator = GetComponent<Animator>();
+        CurrentState = PreviousState = EnemyStates.PURSUE;
+    }
+
+    void Update()
     {
-		switch(CurrentState)
+        UpdateAnimation();
+        navMesh.isStopped = true;
+        switch (CurrentState)
         {
             case EnemyStates.PURSUE:
-                GoTowardsUnit(GetClosestUnitPosition());
+                var pos = GetClosestUnitPosition();
+                if (Vector3.Distance(transform.position, pos) < attackDistance)
+                {
+                    CurrentState = EnemyStates.ATTACK;
+                }
+                else
+                {
+                    GoTowardsUnit(pos);
+                }
                 break;
+            case EnemyStates.ATTACK:
+             
+                var unit = GetClosestUnit();
+                if (Vector3.Distance(transform.position, unit.transform.position) > attackDistance)
+                {
+                    CurrentState = EnemyStates.PURSUE;
+                }
+                attackCycleTime += Time.deltaTime;
+                if (attackCycleTime > 0.4F)
+                {
+                    attackCycleTime = 0F;
+                    //Attack
+                }
+                break;
+            case EnemyStates.DIE:
+
             default:
                 break;
         }
-	}
+    }
+
+    void UpdateAnimation()
+    {
+        isRunning = false;
+        isAttacking = false;
+        isIdle = false;
+        isDead = false;
+        switch (CurrentState)
+        {
+            case EnemyStates.PURSUE:
+                isRunning = true;
+                break;
+            case EnemyStates.ATTACK:
+                isAttacking = true;
+                break;
+            case EnemyStates.DIE:
+                isDead = true;
+                break;
+            default:
+                isIdle = true;
+                break;
+        }
+
+        animator.SetBool("Attack", isAttacking);
+        animator.SetBool("Run", isRunning);
+        animator.SetBool("Die", isDead);
+        animator.SetBool("Idle", isIdle);
+    }
+
+    UnitBehavior GetClosestUnit()
+    {
+        var units = GameObject.FindObjectsOfType<UnitBehavior>();
+        var distance = 9000F;
+        UnitBehavior closestUnit = null;
+        foreach (var unit in units)
+        {
+            var currentDistance = Vector3.Distance(transform.position, unit.transform.position);
+            if (currentDistance < distance)
+            {
+                closestUnit = unit;
+                distance = currentDistance;
+            }
+        }
+
+        return closestUnit;
+    }
 
     void GoTowardsUnit(Vector3 position)
     {
+        navMesh.isStopped = false;
         navMesh.destination = position;
     }
 
@@ -44,10 +131,10 @@ public class EnemyBehavior : MonoBehaviour {
         var units = GameObject.FindObjectsOfType<UnitBehavior>();
         var distance = 9000F;
         UnitBehavior closestUnit = null;
-        foreach(var unit in units)
+        foreach (var unit in units)
         {
             var currentDistance = Vector3.Distance(transform.position, unit.transform.position);
-            if(currentDistance<distance)
+            if (currentDistance < distance)
             {
                 closestUnit = unit;
                 distance = currentDistance;
--- Assets/Scripts/UnitBehavior.cs
+++ Assets/Scripts/UnitBehavior.cs
@@ -11,32 +11,146 @@ enum UnitStates
     DIE
 };
 
-public class UnitBehavior : MonoBehaviour {
-
+public class UnitBehavior : MonoBehaviour
+{
+    Animator animator;
     NavMeshAgent navMesh;
+
+    public GameObject muzzleFlash;
+    public float baseDistanceOffset = 3F;
+    public float Health = 100F;
+    public float attackVisionDistance = 20F;
 
     UnitStates CurrentState;
     UnitStates PreviousState;
 
-    void Start () {
+    bool isIdle = true;
+    bool isAttacking = false;
+    bool isRunning = false;
+    bool isDead = false;
+
+    bool isMoving = false;
+    float attackCycleTime = 0F;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
         navMesh = GetComponent<NavMeshAgent>();
-        CurrentState = PreviousState = UnitStates.GO_TO_NEXT_BASE;
-	}
-	
-	void Update () {
-        switch(CurrentState)
+        CurrentState = PreviousState = UnitStates.GUARD;
+    }
+
+    void Update()
+    {
+        UpdateAnimation();
+        navMesh.isStopped = true;
+        switch (CurrentState)
         {
             case UnitStates.GO_TO_NEXT_BASE:
                 GotoDestination(GetNextBasePosition());
+                AttackEnemyIfClose();
                 break;
             case UnitStates.GUARD:
                 var guardPos = GetClosestGuardPosition();
-                GotoDestination(guardPos);
+                var isEnemyClose = AttackEnemyIfClose();
+                if (Vector3.Distance(transform.position, guardPos) > baseDistanceOffset && !isEnemyClose)
+                {
+                    isMoving = true;
+                    GotoDestination(guardPos);
+                }
+                else
+                {
+                    isMoving = false;
+                }
+                break;
+            case UnitStates.ATTACK:
+                attackCycleTime += Time.deltaTime;
+                var enemy = GetClosestEnemy();
+                if (enemy == null)
+                {
+                    CurrentState = PreviousState;
+                    break;
+                }
+                
+                transform.LookAt(enemy.transform);
+                if (attackCycleTime > 0.4F)
+                {
+                    attackCycleTime = 0F;
+                    var pos = transform.position;
+                    pos.y += 2;
+                    pos = pos + transform.forward * 1;
+                    var flash = Instantiate(muzzleFlash, pos, transform.rotation);
+                    
+                    //Attack
+                }
+                break;
+            case UnitStates.DIE:
                 break;
             default:
                 break;
         }
-	}
+    }
+
+    void UpdateAnimation()
+    {
+        isRunning = false;
+        isAttacking = false;
+        isIdle = false;
+        isDead = false;
+        switch (CurrentState)
+        {
+            case UnitStates.GO_TO_NEXT_BASE:
+                isRunning = true;
+                break;
+            case UnitStates.ATTACK:
+                isAttacking = true;
+                break;
+            case UnitStates.DIE:
+                isDead = true;
+                break;
+            case UnitStates.GUARD:
+                isRunning = isMoving;
+                isIdle = !isMoving;
+                break;
+            default:
+                isIdle = true;
+                break;
+        }
+
+        animator.SetBool("Attack", isAttacking);
+        animator.SetBool("Run", isRunning);
+        animator.SetBool("Die", isDead);
+        animator.SetBool("Idle", isIdle);
+    }
+
+    EnemyBehavior GetClosestEnemy()
+    {
+        var distance = 9000F;
+        var enemies = GameObject.FindObjectsOfType<EnemyBehavior>();
+        EnemyBehavior closestEnemy = null;
+        foreach (var enemy in enemies)
+        {
+            var currentDistance = Vector3.Distance(transform.position, enemy.transform.position);
+            if (currentDistance < attackVisionDistance && currentDistance < distance)
+            {
+                distance = currentDistance;
+                closestEnemy = enemy;
+            }
+        }
+        return closestEnemy;
+    }
+
+    bool AttackEnemyIfClose()
+    {
+        var closestEnemy = GetClosestEnemy();
+        if (closestEnemy != null)
+        {
+            PreviousState = CurrentState;
+            CurrentState = UnitStates.ATTACK;
+            return true;
+        }
+
+        return false;
+    }
 
     Vector3 GetNextBasePosition()
     {
@@ -45,11 +159,12 @@ public class UnitBehavior : MonoBehaviour {
 
     Vector3 GetClosestGuardPosition()
     {
-        return GameObject.Find("BaseMiddle").transform.position; 
+        return GameObject.Find("BaseMiddle").transform.position;
     }
 
     void GotoDestination(Vector3 position)
     {
+        navMesh.isStopped = false;
         navMesh.destination = position;
     }
 }
