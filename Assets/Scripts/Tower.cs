19f74e3ff715254ed8a337bd4e35772dc0798810
Nitish Victor <nithishvictor@gmail.com>
2018-04-21 19:23:14 -0400

    Progress bar

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -2,49 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
-public class Tower : MonoBehaviour {
+public class Tower : MonoBehaviour
+{
 
     public float delay = 5f;
     public float currentTime = 0F;
     public bool IsReady { get; private set; }
     public GameObject fillBarPrefab;
     //FillBar fillBar;
-
     NavMeshObstacle navMeshObstacle;
+    public Canvas canvas;
 
     void Awake()
     {
         navMeshObstacle = GetComponent<NavMeshObstacle>();
         navMeshObstacle.enabled = false;
         IsReady = false;
+        StartCoroutine(Run());
     }
-	// Use this for initialization
-	void Start () {
-        //GameObject obj = Instantiate(fillBarPrefab, transform.position + 7.0f * Vector3.up, Quaternion.identity) as GameObject;
-        //fillBar = obj.GetComponent<FillBar>();
-	}
+    // Use this for initialization
+    void Start()
+    {
+    }
 
     public void StartBuilding()
     {
-        StartCoroutine(Run());
+
     }
 
-
-    private void Update()
+    void Update()
     {
-        
+        var fillbar = fillBarPrefab.GetComponent<RectTransform>();
+        if (currentTime >= delay)
+            canvas.enabled = false;
+        else
+            fillbar.sizeDelta = new Vector2((currentTime / delay) * 10, 1);
     }
 
     IEnumerator Run()
     {
-        for(float i = 0f; i < delay; i += Time.deltaTime)
+        for (float i = 0f; i < delay; i += Time.deltaTime)
         {
             currentTime = i;
-            //fillBar.SetScale(i / delay);
             yield return 0;
         }
-        currentTime = delay;
+        currentTime = delay + 0.01F;
         IsReady = true;
         navMeshObstacle.enabled = true;
     }
