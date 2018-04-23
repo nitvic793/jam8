ede6a4cb084bc91d6ebdc908798c59682f6ed744
Satch Puri <sp1796@g.rit.edu>
2018-04-23 10:34:51 -0400

    Fixes to Audio Manager and HUD

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-
+    
     public AudioSource efxSource;
+    //roger sound
     public AudioSource rogerSource;
+    //background sound
     public AudioSource musicSource;
+    //shoot sound
     public AudioSource shootSource;
+    //
     public AudioSource wobbleSource;
+
     public static AudioManager instance = null;
+    //Pitch Values
     public float lowPitchRange = .95f;
     public float highPitchRange = 1.05f;
+
+    //Volume Values
+    public float volumeLow = 0.11f;
+    public float volumeHigh = 0.22f;
 
     void Awake()
     {
@@ -28,7 +38,7 @@ public class AudioManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-
+    //Play file ones
     public void PlaySingle(AudioClip clip)
     {
         efxSource.clip = clip;
@@ -65,7 +75,9 @@ public class AudioManager : MonoBehaviour
     public void RandomizeRogerSfx(params AudioClip[] clips)
     {
         float randomPitch = Random.Range(lowPitchRange, highPitchRange);
+      
         rogerSource.pitch = randomPitch;
+       
         int randomIndex = Random.Range(0, clips.Length);
         rogerSource.clip = clips[randomIndex];
         rogerSource.PlayOneShot(rogerSource.clip);
@@ -74,7 +86,11 @@ public class AudioManager : MonoBehaviour
     public void RandomizeShootSfx(params AudioClip[] clips)
     {
         float randomPitch = Random.Range(lowPitchRange, highPitchRange);
+        float randomVolume = Random.Range(volumeLow,volumeHigh);
+
         shootSource.pitch = randomPitch;
+        shootSource.volume = randomVolume;
+
         int randomIndex = Random.Range(0, clips.Length);
         shootSource.clip = clips[randomIndex];
         shootSource.PlayOneShot(shootSource.clip);
